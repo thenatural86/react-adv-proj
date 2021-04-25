0abc96e37d119ba0fdc87407c1b40ca7bf8d0ed0
thenatural86 <philip.smalls86@gmail.com>
2021-04-25 19:23:13 -0400

    centrilized state management

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useContext } from 'react'
 
 import Login from './components/Login/Login'
 import Home from './components/Home/Home'
@@ -6,33 +6,15 @@ import MainHeader from './components/MainHeader/MainHeader'
 import AuthContext from './context/auth-context'
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
-
-  useEffect(() => {
-    const userIsLoggedIn = localStorage.getItem('isLoggedIn')
-    if (userIsLoggedIn === '1') {
-      setIsLoggedIn(true)
-    }
-  }, [])
-
-  const loginHandler = (email, password) => {
-    localStorage.setItem('isLoggedIn', '1')
-    setIsLoggedIn(true)
-  }
-
-  const logoutHandler = () => {
-    localStorage.removeItem('isLoggedIn')
-    setIsLoggedIn(false)
-  }
-
+  const ctx = useContext(AuthContext)
   return (
-    <AuthContext.Provider value={{ isLoggedIn, onLogout: logoutHandler }}>
+    <>
       <MainHeader />
       <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
+        {!ctx.isLoggedIn && <Login />}
+        {ctx.isLoggedIn && <Home />}
       </main>
-    </AuthContext.Provider>
+    </>
   )
 }
 
